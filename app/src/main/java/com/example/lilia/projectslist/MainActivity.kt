8e64e7a4862eb5d0d033925f47063b33b92365aa
Pakopac <lilian.pacaud@supinternet>
2019-01-15 01:03:13 +0100

    Added tags and images

--- app/src/main/java/com/example/lilia/projectslist/MainActivity.kt
+++ app/src/main/java/com/example/lilia/projectslist/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lilia.projectslist;
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -8,7 +10,10 @@ import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,9 +23,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val projects = listOf(
-            Project(getString(R.string.name1),getString(R.string.descr1)),
-            Project(getString(R.string.name2),getString(R.string.descr2)),
-            Project(getString(R.string.name3),getString(R.string.name3))
+            Project(getString(R.string.name1),getString(R.string.descr1),
+                getString(R.string.tagName1), resources.getColor(R.color.redTag),
+                "http://i.imgur.com/DvpvklR.png"
+                ),
+
+            Project(getString(R.string.name2), getString(R.string.descr2),
+                getString(R.string.tagName2), resources.getColor(R.color.orangeTag),
+                "http://i.imgur.com/DvpvklR.png"),
+
+            Project(getString(R.string.name3),getString(R.string.name3),
+                getString(R.string.tagName3), resources.getColor(R.color.greenTag),
+                "http://i.imgur.com/DvpvklR.png")
         )
 
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -28,7 +42,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-data class Project(val name:String, val descriptor: String)
+data class Project(val name:String, val description: String, val tagName: String, val colorTag: Int, val image: String)
 
 class MyAdapter(val projects: List<Project>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
     override fun onBindViewHolder(p0: MyViewHolder, p1: Int) {
@@ -48,10 +62,16 @@ class MyAdapter(val projects: List<Project>) : RecyclerView.Adapter<MyAdapter.My
 
         val title: TextView = itemView.findViewById(R.id.title)
         val description: TextView = itemView.findViewById(R.id.description)
+        val tagName: TextView = itemView.findViewById(R.id.textTag)
+        val colorTag: LinearLayout = itemView.findViewById(R.id.blockTag)
+        val image: ImageView = itemView.findViewById(R.id.image)
 
         fun bindValue(project: Project) {
             title.text = project.name
-            description.text = project.descriptor
+            description.text = project.description
+            tagName.text = project.tagName
+            DrawableCompat.setTintList(colorTag.background, ColorStateList.valueOf(project.colorTag))
+            Picasso.get().load(project.image).into(image)
         }
 
     }
